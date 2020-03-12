f478226ba5c4b1c36176078444e82e72e1bd764f
HowardHowonYu <yoohowon@gmail.com>
2020-03-12 17:51:14 +0900

    edit README.md

--- rocketpunch_crawler/rocketpunch_crawler/spiders/rocketpunch_spider.py
+++ rocketpunch_crawler/rocketpunch_crawler/spiders/rocketpunch_spider.py
@@ -1,5 +1,4 @@
 import scrapy
-from scrapy.selector import Selector
 import requests
 from bs4 import BeautifulSoup
 import json
@@ -8,49 +7,23 @@ from rocketpunch_crawler.items import RocketpunchCrawlerItem
 class RocketpunchSpider(scrapy.Spider):
     name = "Rocketpunch"
     allowed_domains = ['https://www.rocketpunch.com']
-    start_urls = []
     
-    # 검색어 마다 페이지가 다르기 때문에, 총 페이지 수를 먼저 체크하고 각 페이지의 URL을 'start_urls' 변수에 리스트 형태로 담는다.
-    url = "https://www.rocketpunch.com/api/jobs/template?page=&q=&specialty=Python&tag=%EB%8D%B0%EC%9D%B4%ED%84%B0"
-    response = requests.get(url)
-    data = response.json()['data']['template']
-    dom = BeautifulSoup(data, "html.parser")
-    total_pages = dom.select("div.ui.pagination.menu > div.tablet.computer.large.screen.widescreen.only > a")[-1].text
-    
-    for page in range(1,int(total_pages)+1):
-        url = "https://www.rocketpunch.com/api/jobs/template?page={}&q=&specialty=Python&tag=%EB%8D%B0%EC%9D%B4%ED%84%B0".format(page)
-        start_urls.append(url)
-    
-    
-# 멀티 쓰레드로 돌리려면 __init__ 안에서 start_url을 찾는짓을 해주자.
+    def __init__(self, specialty="python", tag="데이터", page=1, **kwargs):
+        url_for_get_page = "https://www.rocketpunch.com/api/jobs/template?q=&specialty={}&tag={}&page={}".format(specialty, tag, page)
+        response = requests.get(url_for_get_page)
+        data = response.json()['data']['template']
+        dom = BeautifulSoup(data, "html.parser")
+        total_pages = dom.select("div.ui.pagination.menu > div.tablet.computer.large.screen.widescreen.only > a")[-1].text
 
-# 수업 참조
-#  def parse(self,response):
-#         selector = '//*[@id="gBestWrap"]/div/div[3]/div[2]/ul/li/a/@href'
-#         links = links = response.xpath(selector).extract()
-#         for link in links:
-#             yield scrapy.Request(link,callback=self.get_content)
-#             # 여기서 link로 Request한걸 콜백 함수get_content로 던져준다
+        self.start_urls = []
 
-
-# 순서대로 저장하려면 rank값 줘서 나중에 소팅해보자
-    
-    def __init__(self):
-        # 기존 Spider 클래스의 생성자를 초기화 해준다.
-        scrapy.Spider.__init__(self)
+        for page in range(1,int(total_pages)+1):
+            url = "https://www.rocketpunch.com/api/jobs/template?q=&specialty={}&tag={}&page={}".format(specialty, tag, page)
+            self.start_urls.append(url)           
+        super().__init__(**kwargs)
         
-        company_name = scrapy.Field()
-        deadline = scrapy.Field()
-        business  = scrapy.Field()
-        link = scrapy.Field()
-        position = scrapy.Field()
-        job_condition = scrapy.Field()
-        keyword = scrapy.Field()
-
-    
         
     def parse(self, response):
-#       response = requests.get(url)
         jsonresponse = json.loads(response.body)
         data = jsonresponse['data']['template']       
         dom = BeautifulSoup(data, "html.parser")
